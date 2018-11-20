b7c6b3cc48f0034466c01442399b4644b140dc7c
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-20 17:17:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   var rankedLists = document.querySelectorAll('ul.ranked-list li');
-  for (i=0; i < rankedLists.length; i++) {
+  for (var i=0; i < rankedLists.length; i++) {
     rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
   }
 }
