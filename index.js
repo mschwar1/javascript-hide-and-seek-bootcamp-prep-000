82b0d88262c2657cc295298e14d6a6b681f412e7
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-20 17:41:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('ul.ranked-list li');
+  for (var i=0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var digging = document.querySelectorAll('#grand-node div');
+  return digging[digging.length-1];
+  }
